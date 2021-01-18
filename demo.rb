1186edfb451e979df0de6c222e8e03f573a6f662
Leonidas Apostolidis <leoapost@gmail.com>
2021-01-18 14:13:22 +0000

    Create a demo app
    to run the solution and print the results.

---
+++ demo.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Dir[File.join(__dir__, 'lib', '*.rb')].sort.each { |file| require file }
+
+api_client = ForensicsApiClient.new
+directions = api_client.fetch_directions
+
+locator = Locator.new(directions)
+x, y = locator.locate
+
+results = api_client.search_location(x, y)
+
+puts results
