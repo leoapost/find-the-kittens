b2ed7226376812cf11a8b0e25041b929a38e3aca
Leonidas Apostolidis <leoapost@gmail.com>
2021-01-18 14:21:29 +0000

    Call the correct method to fetch the instruction in the demo app

--- demo.rb
+++ demo.rb
@@ -3,7 +3,7 @@
 Dir[File.join(__dir__, 'lib', '*.rb')].sort.each { |file| require file }
 
 api_client = ForensicsApiClient.new
-directions = api_client.fetch_directions
+directions = api_client.fetch_instructions
 
 locator = Locator.new(directions)
 x, y = locator.locate
